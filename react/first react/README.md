4b075d2fcd2289b3b1a20d585eeb445a3406acca
laxmi kanta poudel <justkantapoudel@gmail.com>
2022-09-28 12:06:38 +0545

    commiting render

---
+++ react/first react/README.md
@@ -0,0 +1,1 @@
+in this file we learned about render in react. 
--- react/first react/index.js
+++ react/first react/index.js
@@ -2,9 +2,3 @@ ReactDOM.render(<h1>Hello everyone</h1>, document.getElementById("root"))
 
 
 ReactDOM.render( <p> <ul><li>one</li><li>two</li><li>Three</li></ul></p>, document.getElementById("here"))
-
-
-ReactDOM.render(
-    <navbar />,
-    document.getElementById("root")
-)
