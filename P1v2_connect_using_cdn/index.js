8252bcab6b61c88e4edbfd81b8756dff38dd8e82
laxmi kanta poudel <justkantapoudel@gmail.com>
2022-10-08 05:47:08 +0545

    adding cdn and composables

---
+++ P1v2_connect_using_cdn/index.js
@@ -0,0 +1,16 @@
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<p>this is a praragraph.</p>,document.getElementById("here"))
