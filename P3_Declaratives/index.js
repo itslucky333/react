ec77b990843bbe0b0c9bd7555e0c12064367cace
laxmi kanta poudel <justkantapoudel@gmail.com>
2022-10-08 09:00:10 +0545

    imperative and declarative

--- P3_Declaratives/index.js
+++ P3_Declaratives/index.js
@@ -1,4 +1,4 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1 className="header">Hello, React!</h1>, document.getElementById("root"))
 
 /* 
 Challenge - recreate the above line of code in vanilla JS by creating and
@@ -13,7 +13,30 @@ appending an h1 to our div#root (without using innerHTML).
 
 
 
-const h1=document.createElement("h1")
-h1.textContent="this is the imperative way"
-h1.ClassName="header"
-document.getElementById("root").append(h1)
+// const h1=document.createElement("h1")
+// h1.textContent="this is the imperative way"
+// h1.ClassName="header"
+// document.getElementById("root").append(h1)
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const bar=( <nav>
+    <h1>this is the title of website </h1>
+    <ul>
+    <li>pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+    </ul>
+    </nav>
+)
+ReactDOM.render(bar, document.getElementById("root"))
