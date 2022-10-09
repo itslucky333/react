0bcc9eca2fe5ca885f30b6ed52d4416d9707f0e7
laxmi kanta poudel <justkantapoudel@gmail.com>
2022-10-09 17:57:45 +0545

    Airbnb clone part 1

---
+++ P14_Airbnb_clone_part1/coec246e5bb037f56b74822b1/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./component/Navbar"
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+        <Navbar />
+        <h1>App component</h1>
+        </div>
+    )
+}
